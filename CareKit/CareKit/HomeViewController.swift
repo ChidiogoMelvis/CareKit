0be6cc009188a9bca2b1ba3359252f7ad5c7c397
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-05-08 01:06:01 +0100

    added side bar

---
+++ CareKit/CareKit/HomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  HomeViewController.swift
+//  CareKit
+//
+//  Created by Mac on 07/05/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let sidebarWidth: CGFloat = 250
+    var sidebarShowing = false
+    var sidebarViewController = SideBarViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        let sidebarButton = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal"), style: .plain, target: self, action: #selector(toggleSidebar))
+        sidebarButton.tintColor = .black
+        navigationItem.leftBarButtonItem = sidebarButton
+    }
+    
+    @objc func toggleSidebar() {
+        if sidebarShowing {
+            hideSidebar()
+        } else {
+            showSidebar()
+        }
+    }
+    
+    func showSidebar() {
+        // Create a sidebar view controller and add it as a child view controller
+        sidebarViewController = SideBarViewController()
+        addChild(sidebarViewController)
+        view.addSubview(sidebarViewController.view)
+        
+        // Position the sidebar off screen
+        sidebarViewController.view.frame = CGRect(x: -sidebarWidth, y: 0, width: sidebarWidth, height: view.frame.height)
+        
+        // Animate the sidebar onto the screen
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarViewController.view.frame = CGRect(x: 0, y: 0, width: self.sidebarWidth, height: self.view.frame.height)
+        }
+        
+        sidebarShowing = true
+    }
+    
+    func hideSidebar() {
+        // Animate the sidebar off screen
+        UIView.animate(withDuration: 0.3, animations: {
+            self.sidebarViewController.view.frame = CGRect(x: CGFloat(-self.sidebarWidth), y: 0, width: self.sidebarWidth, height: self.view.frame.height)
+        }) { (completed) in
+            // Remove the sidebar view controller from the parent view controller
+            self.sidebarViewController.view.removeFromSuperview()
+            self.sidebarViewController.removeFromParent()
+            //self.sidebarViewController = nil
+        }
+        
+        sidebarShowing = false
+    }
+    
+}
---
+++ CareKit/CareKit/SideBarViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SideBarViewController.swift
+//  CareKit
+//
+//  Created by Mac on 07/05/2023.
+//
+
+import UIKit
+
+class SideBarViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let items = ["Item 1", "Item 2", "Item 3"]
+    var didSelectItem: ((String) -> Void)?
+    
+    // Create and configure the table view
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Menu"
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Table view data source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Call the completion block with the selected item
+        let selectedItem = items[indexPath.row]
+        didSelectItem?(selectedItem)
+    }
+    
+}
+
+
