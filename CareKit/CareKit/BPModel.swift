b2a55adbfb1d5ebd77bfea23cbf1ead9a0aadd3f
Agbachi Chidiogo Melvis <86145394+ChidiogoMelvis@users.noreply.github.com>
2023-06-20 20:30:24 +0100

    updated

---
+++ CareKit/CareKit/BPModel.swift
@@ -0,0 +1,16 @@
+//
+//  BPModel.swift
+//  CareKit_
+//
+//  Created by Mac on 20/06/2023.
+//
+
+import Foundation
+
+struct BPReading {
+    var systolic: Double = 120
+    var diastolic: Double = 80
+    var date = Date()
+    
+    //let bpReading = BPReading(systolic: systolic, diastolic: diastolic, date: date.self)
+}
